fbe9079880bb7a502292fe3683a351836a8ada96
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-11-28 22:32:47 -0800

    Added a decrement button and an error message if the count is going to be less than 0

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,44 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
+      error: false
     };
   }
 
   incrementCount = event => {
     this.setState({
-      count: this.state.count + 1
+      count: this.state.count + 1,
+      error: false
     });
+  };
+
+  decrementCount = event => {
+    if (this.state.count === 0) {
+      this.setState({ error: true });
+    } else if (this.state.count > 0) {
+      this.setState({
+        count: this.state.count - 1
+      });
+    }
   };
 
   render() {
     return (
       <div className="App" data-test="component-app">
-        <h2>Hello world!</h2>
+        <h2>Counter App</h2>
         <h1 data-test="component-display">{this.state.count}</h1>
-        <button onClick={this.incrementCount} data-test="component-button">
-          +
-        </button>
+        {this.state.error ? (
+          <h2 data-test="error-message">Counter cannot be less than 0</h2>
+        ) : null}
+        <div>
+          <button onClick={this.incrementCount} data-test="increment-button">
+            +
+          </button>
+          <button onClick={this.decrementCount} data-test="decrement-button">
+            -
+          </button>
+        </div>
       </div>
     );
   }
